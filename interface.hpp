2e8f2c161ce1bd85a6632e5846bdc390e3cee54a
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-04-10 14:24:17 +0200

    More work on interfaces.

--- interface.hpp
+++ interface.hpp
@@ -1,3 +1,4 @@
+#include <istream>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -9,14 +10,30 @@
 class AnnotatedTree {
   public:
     using NodeIndex = int;
+    using TagName = std::string;
+    using TagValue = std::string;
+    using ChildrenList = std::vector<NodeIndex>;
 
-    virtual const std::vector<int>& children(NodeIndex node) const = 0;
+    virtual const ChildrenList& children(NodeIndex) const = 0;
 
-    virtual int parent(NodeIndex node) const = 0;
+    virtual NodeIndex parent(NodeIndex) const = 0;
+
+    virtual NodeIndex root() const = 0;
 
     virtual std::size_t nb_nodes() const = 0;
 
-    virtual std::string tag(NodeIndex node, std::string tag) const = 0;
+    virtual TagValue tag(NodeIndex, TagName) const = 0;
+};
+
+class TreeParser {
+  public:
+    // result guaranteed to be valid until next parse call
+    virtual const AnnotatedTree& parse(std::istream&) = 0;
+};
+
+class TreeWriter {
+  public:
+    virtual void write(std::ostream&, const AnnotatedTree&) = 0;
 };
 
 /*
@@ -28,22 +45,29 @@ class DoubleListAnnotatedTree : public AnnotatedTree {
 
     std::vector<Node> nodes_;
 
-    // (invariant: same length as nodes) element i is the index of
-    // parent of i in nodes (-1 for root)
+    // invariant: same length as nodes
+    // invariant: only one node has parent -1
+    // element i is the index of parent of i in nodes (-1 for root)
     std::vector<int> parent_;
 
-    // (invariant: consistent with parent) element i is a vector of
-    // indices corresponding to the children of node i
+    // invariant: consistent with parent
+    // element i is a vector of indices corresponding to the children of node i
     std::vector<std::vector<int>> children_;
 
+    // invariant: node with index root is only node with parent -1
+    NodeIndex root_;
+
   public:
-    using NodeIndex = int;
+    const ChildrenList& children(NodeIndex node) const final { return children_.at(node); }
 
-    const std::vector<int>& children(NodeIndex node) const final { return children_.at(node); }
+    NodeIndex parent(NodeIndex node) const final { return parent_.at(node); }
 
-    int parent(NodeIndex node) const final { return parent_.at(node); }
+    NodeIndex root() const final { return root_; }
 
     std::size_t nb_nodes() const final { return nodes_.size(); }
 
-    std::string tag(NodeIndex node, std::string tag) const final { return nodes_.at(node).at(tag); }
+    TagValue tag(NodeIndex node, TagName tag) const final { return nodes_.at(node).at(tag); }
 };
+
+/*================================================================================================*/
+class NHXParser : public TreeParser {};
