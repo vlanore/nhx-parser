a980d7664ba05f3598b24dec79709b4d2e910699
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-04-19 16:30:24 +0200

    Added Cecill-B license + headers + updated old-test.

--- src/old-test.cpp
+++ src/old-test.cpp
@@ -1,3 +1,30 @@
+/*Copyright or Copr. Centre National de la Recherche Scientifique (CNRS) (2018)
+Contributors:
+- Vincent Lanore <vincent.lanore@gmail.com>
+
+This software is a computer program whose purpose is to provide a header-only standalone parser for
+NHX (New Hampshire Extended) phylogenetic trees.
+
+This software is governed by the CeCILL-C license under French law and abiding by the rules of
+distribution of free software. You can use, modify and/ or redistribute the software under the terms
+of the CeCILL-C license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info".
+
+As a counterpart to the access to the source code and rights to copy, modify and redistribute
+granted by the license, users are provided only with a limited warranty and the software's author,
+the holder of the economic rights, and the successive licensors have only limited liability.
+
+In this respect, the user's attention is drawn to the risks associated with loading, using,
+modifying and/or developing or reproducing the software by the user in light of its specific status
+of free software, that may mean that it is complicated to manipulate, and that also therefore means
+that it is reserved for developers and experienced professionals having in-depth computer knowledge.
+Users are therefore encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or data to be ensured and,
+more generally, to use and operate it in the same conditions as regards security.
+
+The fact that you are presently reading this means that you have had knowledge of the CeCILL-C
+license and that you accept its terms.*/
+
 #include <chrono>
 #include <fstream>
 #include <iostream>
@@ -11,15 +38,14 @@ int main() {
     stringstream ss{
         "(((ADH2:0.1[&&NHX:S=human:E=1.1.1.1], ADH1:0.11[&&NHX:S=human:E=1.1.1.1]):0.05[&&NHX:S="
         "Primates:E=1.1.1.1:D=Y:B=100],ADHY:0.1[&&NHX:S=nematode:E=1.1.1.1],ADHX:0.12[&&NHX:S="
-        "insect:E=1.1.1.1]):0.1[&&NHX:S=Metazoa:E==++1.1.1.1:D=N],(ADH4:0.09[&&NHX:S=yeast:E=1.1.1."
-        "1],"
+        "insect:E=1.1.1.1]):0.1[&&NHX:S=Metazoa:E=1.1.1.1:D=N],(ADH4:0.09[&&NHX:S=yeast:E=1.1.1.1],"
         "ADH3:0.13[&&NHX:S=yeast:E=1.1.1.1],ADH2:0.12[&&NHX:S=yeast:E=1.1.1.1],ADH1:0.11[&&NHX:S="
         "yeast:E=1.1.1.1]):0.1[&&NHX:S=Fungi])[&&NHX:E=1.1.1.1:D=N];"};
 
-    NHXParser parser;
+    NHXParser parser(ss);
 
     auto begin = std::chrono::high_resolution_clock::now();
-    auto& tree = parser.parse(ss);
+    auto& tree = parser.get_tree();
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms"
               << std::endl;
